9c7d7b03c883964b491d79a2eca2485b22ffd2ad
José Thomaz <jose.thoomaz@gmail.com>
2024-12-28 17:02:35 -0300

    create presentation

--- frontend-loteria/src/App.tsx
+++ frontend-loteria/src/App.tsx
@@ -1,15 +1,15 @@
 import Header from "./components/header/Header";
 import Presentation from "./components/presentation/Presentation";
 
+
 function App() {
   return (
     <>
       <Header />
       <Presentation />
-      <main>
-      </main>
+
     </>
   );
-};
+}
 
 export default App;
--- frontend-loteria/src/components/presentation/Presentation.tsx
+++ frontend-loteria/src/components/presentation/Presentation.tsx
@@ -1,13 +1,20 @@
+import "./Presentation.css";
+
 function Presentation() {
-    return (
-        <div>
-          <div className="circulos">
-            <div className="circulo_maior"></div>
-            <div className="circulo_medio"></div>
-            <div className="circulo_menor"></div>
-          </div>
-        </div>
-    )
+  return (
+    <div className="circle-section">
+      <h1 className="text-presentation title">Loteria</h1>
+
+      <div className="text-presentation slogan">
+        <p className="one">agilize o processo</p>
+        <p className="two">verifique seu bolão <a href="https://youtu.be/dQw4w9WgXcQ?si=mxaXf9FK-tcH9S7E" target="_blank" rel="noopener noreferrer" className="highlight">AQUI</a></p>
+      </div>
+
+      <div className="circle large-circle"></div>
+      <div className="circle medium-circle"></div>
+      <div className="circle small-circle"></div>
+    </div>
+  );
 }
 
 export default Presentation;
